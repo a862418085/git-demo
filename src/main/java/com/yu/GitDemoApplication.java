71d0eebfe655ce9365a0021f4685947a95002fe8
guanjun <862418085@qq.com>
2022-05-12 18:03:42 +0800

    first commit

---
+++ src/main/java/com/yu/GitDemoApplication.java
@@ -0,0 +1,14 @@
+package com.yu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitDemoApplication {
+
+    public static void main(String[] args) {
+        System.out.println("添加缓存区");
+        SpringApplication.run(GitDemoApplication.class, args);
+    }
+
+}
