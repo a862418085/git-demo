1816fdaa34c61c092147e98f9b3d4b71261ae780
guanjun <862418085@qq.com>
2022-05-12 18:06:07 +0800

    Seconf commit

--- src/main/java/com/yu/GitDemoApplication.java
+++ src/main/java/com/yu/GitDemoApplication.java
@@ -8,6 +8,7 @@ public class GitDemoApplication {
 
     public static void main(String[] args) {
         System.out.println("添加缓存区");
+        System.out.println("添加缓存区2");
         SpringApplication.run(GitDemoApplication.class, args);
     }
 
